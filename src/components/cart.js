2decea4473aac2807e5f1e5405fa17e95a7f9f98
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-03-22 13:18:32 -0600

    [.] Update Cart Component

--- src/components/cart.js
+++ src/components/cart.js
@@ -23,20 +23,20 @@ const Cart = ({state, dispatch}) => {
 
     return(
         <div className="mainContainer">
-           <b className="bCart"> Cart</b>
+           <b > Cart</b>
            <b>Subtotal: ${total}</b>
-           <div className="cartContainer">
+           <div >
             {cart.length > 0 ?
               cart.map((prod) => (
-                <div key={prod.title} className="prodContainer">
-                   <div className="imgContainer">
-                    <img src={prod.thumbnail} alt={prod.title} className="img"/>
-                    <div className="infContainer">
+                <div key={prod.title} >
+                   <div >
+                    <img src={prod.thumbnail} alt={prod.title} />
+                    <div >
                         <span>{prod.title}</span>
                         <b>$ {prod.price}</b>
                     </div>
                    </div>
-                   <div className="butonsContainer">
+                   <div >
                     <button onClick={() => changeQty(prod.id, prod.qty -1)}>-</button>
                     <span>{prod.qty}</span>
                     <button onClick={() => changeQty(prod.id, prod.qty +1)}>+</button>
